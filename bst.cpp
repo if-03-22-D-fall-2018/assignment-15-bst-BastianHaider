cd14b9b7885b5b100017fc54b04a5b6c0f14c748
BastianHaider <bastian.haider@gmx.at>
2019-03-14 08:58:09 +0100

    finished

--- bst.cpp
+++ bst.cpp
@@ -101,7 +101,7 @@ void add(Bst* bst, int value)
 }
 int root_value(Bst bst)
 {
-  if (bst->element == 0)
+  if (bst == 0)
   {
     return 0;
   }
@@ -125,25 +125,72 @@ Bst right_subtree(Bst root)
 }
 int traverse_pre_order(Bst bst, int *elements, int start)
 {
-  return 0;
+  if (bst!=0)
+  {
+    elements[start]=bst->element;
+    start++;
+    start=traverse_pre_order(bst->left,elements,start);
+    start=traverse_pre_order(bst->right,elements,start);
+  }
+  return start;
 }
 int traverse_in_order(Bst bst, int *elements, int start)
 {
-  return 0;
+  if (bst!=0)
+  {
+    start=traverse_in_order(bst->left,elements,start);
+    elements[start]=bst->element;
+    start++;
+    start=traverse_in_order(bst->right,elements,start);
+  }
+  return start;
 }
 int traverse_post_order(Bst bst, int *elements, int start)
 {
-  return 0;
+  if (bst!=0)
+  {
+    start=traverse_post_order(bst->left,elements,start);
+    start=traverse_post_order(bst->right,elements,start);
+    elements[start]=bst->element;
+    start++;
+  }
+  return start;
 }
 bool are_equal(Bst bst1, Bst bst2)
 {
-  return false;
+  if (bst1==0)
+  {
+    return bst2==0;
+  }
+    else if(get_depth(bst1)==get_depth(bst2))
+    {
+      if (bst1->element==bst2->element) {
+        return are_equal(bst1->left,bst2->left)&&are_equal(bst1->right,bst2->right);
+      }
+    }
+    return false;
 }
 void most_left_longest_branch(Bst bst, Bst* branch)
 {
-
+  if (bst!=0)
+  {
+    if (get_depth(bst->left)>= get_depth(bst->right))
+    {
+      add(branch,bst->element);
+      most_left_longest_branch(bst->left,branch);
+    }
+    else{
+      add(branch,bst->element);
+      most_left_longest_branch(bst->right,branch);
+    }
+  }
 }
 int get_number_of_subtrees(Bst bst)
 {
-  return 0;
+  if (bst!=0)
+  {
+    int *element=new int[get_depth(bst)];
+    return traverse_in_order(bst, element, 0)-1;
+  }
+  return -1;
 }
